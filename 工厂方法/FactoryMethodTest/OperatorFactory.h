e296b049703634627b00da5d3d90dd62dc7d2e98
JBB <980374208@qq.com>
2022-08-24 21:51:12 +0800

    add

---
+++ 工厂方法/FactoryMethodTest/OperatorFactory.h
@@ -0,0 +1,98 @@
+﻿#ifndef OPERATORFACTORY_H
+#define OPERATORFACTORY_H
+
+enum Except{
+    EXCEP_ZERO,  //除数为0
+    EXCEP_ONE   //待定
+};
+
+class Operator{
+public:
+    virtual double Calculate() = 0;
+
+
+    void SetNumA(double a){
+        NumA = a;
+    }
+    void SetNumB(double b){
+        NumB = b;
+    }
+    double NumA;
+    double NumB;
+};
+
+class AddOperator:public Operator{
+public:
+    virtual double Calculate(){
+        return this->NumA+this->NumB;
+    }
+};
+
+class SubOperator:public Operator{
+public:    
+    virtual double Calculate(){
+        return this->NumA-this->NumB;
+    }
+};
+
+class MulOperator:public Operator{
+public:
+    virtual double Calculate(){
+            return this->NumA*this->NumB;
+    }
+};
+
+class DivOperator:public Operator{
+public:
+    virtual double Calculate(){
+        if(NumB == 0){
+            throw EXCEP_ZERO;
+        }else
+        {
+             return this->NumA/this->NumB;
+        }
+    }
+};
+
+
+
+
+
+class IOperator{
+  public:
+  virtual Operator* CreateFactory() = 0;
+};
+
+class AddFactory:public IOperator{
+public:
+    Operator* CreateFactory(){
+        return new AddOperator;
+    }
+};
+
+class MulFactory:public IOperator{
+    public:
+    Operator* CreateFactory(){
+        return new  MulOperator;
+    }
+};
+
+class SubFactory:public IOperator{
+    public:
+    Operator* CreateFactory(){
+        return new SubOperator;
+    }
+};
+
+class DivFactory:public IOperator{
+    public:
+    Operator* CreateFactory(){
+        return new DivOperator;
+    }
+};
+
+
+
+
+
+#endif // OPERATORFACTORY_H
---
+++ 工厂方法/FactoryMethodTest/mainwindow.cpp
@@ -0,0 +1,62 @@
+﻿#pragma execution_character_set("utf-8")
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "OperatorFactory.h"
+
+//using namespace std;
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    m_factory = nullptr;
+}
+
+MainWindow::~MainWindow()
+{
+    if(m_factory){
+        delete m_factory;
+        m_factory = nullptr;
+    }
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    if(ui->cb_operator->currentText() == "+"){
+        if(m_factory){
+            delete m_factory;
+        }
+        m_factory = new AddFactory;
+    }else if(ui->cb_operator->currentText() == "-"){
+        if(m_factory){
+            delete m_factory;
+        }
+        m_factory = new SubFactory;
+    }else if(ui->cb_operator->currentText() == "*"){
+        if(m_factory){
+            delete m_factory;
+        }
+        m_factory = new MulFactory;
+    }else if(ui->cb_operator->currentText() == "/"){
+        if(m_factory){
+            delete m_factory;
+        }
+        m_factory = new DivFactory;
+    }
+
+    Operator* addOpe = m_factory->CreateFactory();
+    addOpe->SetNumA(ui->dSp_a->value());
+    addOpe->SetNumB(ui->dSp_b->value());
+    QT_TRY{
+      ui->le_result->setText(QString::number( addOpe->Calculate()));
+      delete addOpe;
+    }QT_CATCH(Except ex){
+         if(ex == EXCEP_ZERO) {
+             ui->le_result->setText("除数不能为0");
+         };
+    }
+
+}
+
